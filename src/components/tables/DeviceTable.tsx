84ceeb386cb619b4957ef42402b58253ec3ff4c0
Nick Moore <nickmoore_1@live.com>
2023-08-29 22:49:41 -0400

    Building and styling the table

--- src/components/tables/DeviceTable.tsx
+++ src/components/tables/DeviceTable.tsx
@@ -1,24 +1,48 @@
 import * as React from 'react'
 import SearchBar from './elements/SearchBar'
-import listView from '../../assets/icons/list-view.svg'
-import gridView from '../../assets/icons/grid-view.svg'
+import ActionButtonGroup from './elements/ActionButtonGroup'
 
-type Device = {
-  sysids: string[],
-  icon: object,
-  line: object,
-  sysid: string,
-  guids: string[],
-  uisp: object,
-  btle: object,
+type Icon = {
   id: string,
-  product: object,
-  shortname: string[],
-  triplets: object[],
+  resolutions: string[],
 }
 
+type Line = {
+  name: string,
+  id: string,
+}
+
+type Product = {
+  abbrev: string,
+  name: string,
+}
+
+interface Device {
+  sysids: string[];
+  icon: Icon;
+  line: Line;
+  sysid: string;
+  guids: string[];
+  uisp: object;
+  btle: object;
+  id: string;
+  product: Product;
+  shortname: string[];
+  triplets: object[];
+}
+
+interface IDevices extends Array<Device>{}
+
 export default function DeviceTable() {
-  const [devices, setDevices] = React.useState({})
+  const [devices, setDevices] = React.useState<IDevices>([])
+
+  const handleClick = () => {
+    console.log('Button clicked')
+  }
+
+  const handleProductDetails = (device:Device) => {
+    console.log(device)
+  }
 
   React.useEffect(() => {
     fetch("https://static.ui.com/fingerprint/ui/public.json")
@@ -29,17 +53,42 @@ export default function DeviceTable() {
       .catch((error) => console.log(error));
   }, []);
 
+  const deviceTableItems = devices.map((device, index) =>
+    <tr key={ index }
+      onClick={() => handleProductDetails(device) }
+    >
+      <td className='iconCol'>
+        <img src={ `https://static.ui.com/fingerprint/ui/icons/${device.icon.id}_${device.icon.resolutions[0][0]}x${device.icon.resolutions[0][1]}.png` } alt={ `Image of ${device.product.name}` } />
+      </td>
+      <td className='lineCol lineColItem'>{ device.line.name }</td>
+      <td className='productNameCol productNameColItem'>{ device.product.name }</td>
+    </tr>
+  )
+
   return (
-    <div className='flex flex-row nowrap justify-between tableActions'>
-      <div className='flex flex-row nowrap items-center tableSearch'>
-        <SearchBar />
-        <span className='muted'> { Object.keys(devices).length } devices </span>
+    <div className='flex flex-col nowrap devices'>
+      <div className='flex flex-row nowrap justify-between tableActions'>
+        <div className='flex flex-row nowrap items-center tableSearch'>
+          <SearchBar />
+          <span className='muted'> { Object.keys(devices).length } devices </span>
+        </div>
+        <div className='flex flex-row nowrap justify-center items-center actions'>
+          <ActionButtonGroup onClickButton1={ handleClick } onClickButton2={ handleClick } />
+          <button>Filter</button>
+        </div>
       </div>
-      <div className='flex flex-row nowrap justify-center items-center actions'>
-        <img src={ listView } alt='List view icon' />
-        <img src={ gridView } alt='Grid view icon' />
-        <h3>Filter</h3>
-      </div>
+      <table>
+        <thead>
+          <tr>
+            <th className='iconCol'></th>
+            <th className='lineCol'>Product Line</th>
+            <th className='productNameCol'>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          { deviceTableItems }
+        </tbody>
+      </table>
     </div>
   )
 }
---
+++ src/components/tables/elements/ActionButtonGroup.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import listView from '../../../assets/icons/list-view.svg'
+import gridView from '../../../assets/icons/grid-view.svg'
+
+interface ActionButtonGroupProps {
+  onClickButton1: () => void;
+  onClickButton2: () => void;
+}
+
+const ActionButtonGroup: React.FC<ActionButtonGroupProps> = ({ onClickButton1, onClickButton2 }) => {
+  return (
+    <div className='flex flex-row nowrap justify-center items-center'>
+      <button onClick={ onClickButton1 }>
+        <object data={ listView } className='actionIcon active' />
+      </button>
+      <button onClick={ onClickButton2 }>
+        <object data={ gridView } className='actionIcon' />
+      </button>
+    </div>
+  );
+};
+
+export default ActionButtonGroup;
